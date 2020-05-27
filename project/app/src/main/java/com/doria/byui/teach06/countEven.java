c224467e797d101bf6126438901d1ace12844250
Skhoooler <dor15004@byui.edu>
2020-05-27 12:18:40 -0600

    finished countEven class

--- project/app/src/main/java/com/doria/byui/teach06/countEven.java
+++ project/app/src/main/java/com/doria/byui/teach06/countEven.java
@@ -1,8 +1,19 @@
 package com.doria.byui.teach06;
 
 public class countEven implements Runnable {
+
     @Override
     public void run() {
-        
+        for (int i = 0; i <= 100; i++){
+            if (i % 2 == 0)
+            {
+                System.out.println(i);
+                try {
+                    Thread.sleep(250);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }

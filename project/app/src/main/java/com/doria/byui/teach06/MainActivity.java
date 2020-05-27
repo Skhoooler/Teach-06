7c5937890d6490b760a46eaece2a8a7e38eb293f
yugimumoto1 <sonnytoe96@gmail.com>
2020-05-27 15:21:22 -0600

    added toasts

--- project/app/src/main/java/com/doria/byui/teach06/MainActivity.java
+++ project/app/src/main/java/com/doria/byui/teach06/MainActivity.java
@@ -2,11 +2,15 @@ package com.doria.byui.teach06;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+Context context = getApplicationContext();
+int duration = Toast.LENGTH_SHORT;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,10 +19,26 @@ public class MainActivity extends AppCompatActivity {
     public void onEvenButtonPress(View view){
         Thread even = new Thread(new countEven());
         even.start();
+        /*runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                CharSequence text = "Finished Evens";
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });*/
     }
     public void onOddButtonPress(View view){
         Thread odd = new Thread(new OddCount());
         odd.start();
+        /*runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                CharSequence text = "Finished Odds";
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });*/
     }
 
 }

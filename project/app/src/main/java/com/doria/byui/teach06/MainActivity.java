564081208121ef57a1a951f8f6df0eb6ad07da29
yugimumoto1 <sonnytoe96@gmail.com>
2020-05-27 15:55:01 -0600

    finsihed up parts 1 and 2

--- project/app/src/main/java/com/doria/byui/teach06/MainActivity.java
+++ project/app/src/main/java/com/doria/byui/teach06/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-Context context = getApplicationContext();
+
 int duration = Toast.LENGTH_SHORT;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,28 +17,45 @@ int duration = Toast.LENGTH_SHORT;
         setContentView(R.layout.activity_main);
     }
     public void onEvenButtonPress(View view){
-        Thread even = new Thread(new countEven());
-        even.start();
-        /*runOnUiThread(new Runnable() {
+        Thread even = new Thread(new Runnable() {
             @Override
             public void run() {
-                CharSequence text = "Finished Evens";
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.show();
+              countEven even = new countEven();
+              even.run();
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Context context = getApplicationContext();
+                        CharSequence text = "Finished Evens";
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+                    }
+                });
             }
-        });*/
+        });
+        even.start();
     }
     public void onOddButtonPress(View view){
-        Thread odd = new Thread(new OddCount());
-        odd.start();
-        /*runOnUiThread(new Runnable() {
+        Thread odd = new Thread(new Runnable() {
             @Override
             public void run() {
-                CharSequence text = "Finished Odds";
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.show();
+                OddCount odd = new OddCount();
+                odd.run();
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Context context = getApplicationContext();
+                        CharSequence text = "Finished Odds";
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+                    }
+                });
             }
-        });*/
+        });
+        odd.start();
+    }
     }
 
-}
+
--- project/app/src/main/java/com/doria/byui/teach06/OddCount.java
+++ project/app/src/main/java/com/doria/byui/teach06/OddCount.java
@@ -1,11 +1,11 @@
 package com.doria.byui.teach06;
 
-public class OddCount implements Runnable {
+public class OddCount  {
     OddCount(){
 
     };
 
-    @Override
+
     public void run() {
         for(int i = 1; i < 100; i = i+2){
             System.out.println(i);

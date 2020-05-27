134128672d97596b97734a815f07c3a7cf0d43cf
yugimumoto1 <sonnytoe96@gmail.com>
2020-05-27 12:41:59 -0600

    made odd and even threads

--- project/app/src/main/java/com/doria/byui/teach06/MainActivity.java
+++ project/app/src/main/java/com/doria/byui/teach06/MainActivity.java
@@ -3,6 +3,7 @@ package com.doria.byui.teach06;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void onEvenButtonPress(View view){
+        Thread even = new Thread(new countEven());
+        even.start();
+    }
+    public void onOddButtonPress(View view){
+        Thread odd = new Thread(new OddCount());
+        odd.start();
+    }
+
 }
